e0e33ff8ac17c2fe321ef70d9f6e972e7698d320
wadie999 <elamraniwadie99@gmail.com>
2023-07-31 16:25:59 +0200

    Transforming the first version into class and methods

--- llm.py
+++ llm.py
@@ -4,12 +4,29 @@ import keys
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
 
-os.environ["OPENAI_API_KEY"] = keys.key
+class TextSearch:
+    def __init__(self, filename, encoding='utf-8'):
+        self.filename = filename
+        self.encoding = encoding
+        self.index = None
 
-query = sys.argv[1]
-print(query)
+    def load_document(self):
+        loader = TextLoader(self.filename, self.encoding)
+        self.index = VectorstoreIndexCreator().from_loaders([loader])
 
-loader = TextLoader('minidata.txt')
-index = VectorstoreIndexCreator().from_loaders([loader])
+    def search(self, query):
+        if self.index is None:
+            raise ValueError("The document is not Loaded")
 
-print(index.query(query))
+        return self.index.query(query)
+
+if __name__ == "__main__":
+    os.environ["OPENAI_API_KEY"] = keys.key
+
+    query = sys.argv[1]
+    print(query)
+
+    text_search = TextSearch('1_transcript.txt', encoding='utf-8')
+    text_search.load_document()
+    results = text_search.search(query)
+    print(results)
