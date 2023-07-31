c69951689406852ccb2fb5434441e04506ad8388
wadie999 <elamraniwadie99@gmail.com>
2023-07-31 18:04:23 +0200

    defining the text tokenizer as a class

--- tokenization.py
+++ tokenization.py
@@ -2,23 +2,31 @@ import openai
 import tiktoken
 import os 
 
-def read_file_creat_chunks(fname,chunkSize):
-    tt_encoding = tiktoken.get_encoding("cl100k_base")
-    with open(fname,'r', encoding = "utf8") as f :
-        file_text = f.read()
+class TextTokenizer:
+    def __init__(self, encoding):
+        self.encoding = encoding
+        self.tt_encoding = tiktoken.get_encoding(encoding)
+    
+    def read_file(self,fname):
+        with open(fname, 'r', encoding="utf8") as f:
+            file_text = f.read()
+        return file_text
+    
+    def creat_chunks(self, text, chunkSize):
+        tokens = self.tt_encoding.encode(file_text)
+        total_tokens = len(tokens)
 
-    tokens = tt_encoding.encode(file_text)
-    total_tokens = len(tokens)
-
-    chunks = []
-    for i in range(0,total_tokens,chunkSize):
-        chunk = tokens[i:i+chunkSize]
-        chunks.append(chunk)
-    return chunks
+        chunks = []
+        for i in range(0,total_tokens,chunkSize):
+            chunk = tokens[i:i+chunkSize]
+            chunks.append(chunk)
+        return chunks
 
 
-chunks = read_file_creat_chunks('1_transcript.txt',500)
-print(len(chunks))
-print(len(chunks[0]))
-print(len(chunks[2]))
-print(len(chunks[8]))
+if __name__ == "__main__":
+    text_tokenizer = TextTokenizer(encoding='cl100k_base')
+    file_text = text_tokenizer.read_file('1_transcript.txt')
+    chunks = text_tokenizer.creat_chunks(file_text, 500)
+
+    print(len(chunks))
+    print(len(chunks[0]))
