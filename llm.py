11a141ccfa554da286afc4e24ee9400a36097680
wadie999 <elamraniwadie99@gmail.com>
2023-08-01 20:58:55 +0200

    Add methods : split /store / retrieval

--- llm.py
+++ llm.py
@@ -1,24 +1,38 @@
 import os
 import sys
 import keys 
+import tokenization
+
 from langchain.document_loaders import TextLoader
 from langchain.indexes import VectorstoreIndexCreator
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+
 
 class TextSearch:
-    def __init__(self, filename, encoding='utf-8'):
+    def __init__(self, filename, encoding='cl100k_base'):
         self.filename = filename
         self.encoding = encoding
+        self.tokenizer = tokenization.TextTokenizer(encoding)
+        self.text = None
+        self.chunks = None
         self.index = None
 
     def load_document(self):
-        loader = TextLoader(self.filename, self.encoding)
-        self.index = VectorstoreIndexCreator().from_loaders([loader])
+        self.text = self.tokenizer.read_file(self.filename)
 
-    def search(self, query):
-        if self.index is None:
-            raise ValueError("The document is not Loaded")
+    def split_text(self, max_tokens=500):
+        self.chunks = self.tokenizer.creat_chunks(self.text, max_tokens)
+        
+    def store_chunks(self):
+        texts = [chunk for chunk in self.chunks]
+        self.vectorstore = Chroma.from_texts(texts=texts, embedding=OpenAIEmbeddings())
 
-        return self.index.query(query)
+    def retrieve_n_chunks(self, question, n=3):
+        important_chunks = self.vectorstore.similarity_search(question)
+        return important_chunks[:n]
+    
+
 
 if __name__ == "__main__":
     os.environ["OPENAI_API_KEY"] = keys.key
@@ -26,7 +40,9 @@ if __name__ == "__main__":
     query = sys.argv[1]
     print(query)
 
-    text_search = TextSearch('1_transcript.txt', encoding='utf-8')
+    text_search = TextSearch('1_transcript.txt')
     text_search.load_document()
-    results = text_search.search(query)
+    text_search.split_text()
+    text_search.store_chunks()
+    results = text_search.retrieve_n_chunks(query) 
     print(results)
