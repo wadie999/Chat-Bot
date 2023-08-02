f3a707d34a413c2740104ba18308d8354428924a
wadie999 <elamraniwadie99@gmail.com>
2023-08-02 15:56:50 +0200

    Front works

--- static/script.js
+++ static/script.js
@@ -1,12 +1,12 @@
 function sendQuery() {
-    const query = document.getElementById('query').value;
+    const query = document.getElementById('queryInput').value;
     fetch(`http://127.0.0.1:8000/query?query=${query}`)
         .then(response => response.json())
-        .then(data => {
-            document.getElementById('response').innerText = data.response;
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            document.getElementById('response').innerText = 'An error occurred. Please try again.';
-        });
+        .then(data => showResponse(data.response)) // Here you pass the response directly, which is a string
+        .catch(error => console.error('An error occurred:', error));
+}
+
+function showResponse(response) {
+    const responseDiv = document.getElementById('responseDiv');
+    responseDiv.textContent = response.result; // Set the text content to the response string
 }
