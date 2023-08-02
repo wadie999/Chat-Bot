6fb1e31a6eac3aa032800d424745779c5a872f99
wadie999 <elamraniwadie99@gmail.com>
2023-08-02 15:58:26 +0200

    Liaison front avec api

--- src/main.py
+++ src/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI, Query
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import FileResponse
 from .llm import run_query
 from . import keys
 import os
 
-
-
-
 app = FastAPI()
 os.environ["OPENAI_API_KEY"] = keys.key
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def read_root():
+    return FileResponse('static/index.html')
 
 @app.get("/query")
 async def get_query_response(query: str = Query(..., description="Enter your query here")):
