e1110fa7adf1212a30cd77833dcfe05113286452
wadie999 <elamraniwadie99@gmail.com>
2023-08-03 17:09:08 +0200

    Adding chat bubbles to the front

--- static/script.js
+++ static/script.js
@@ -1,12 +1,24 @@
 function sendQuery() {
     const query = document.getElementById('queryInput').value;
+    showQuery(query);
     fetch(`http://127.0.0.1:8000/query?query=${query}`)
         .then(response => response.json())
         .then(data => showResponse(data.response)) // Here you pass the response directly, which is a string
         .catch(error => console.error('An error occurred:', error));
 }
 
+function showQuery(query) {
+    const responseDiv = document.getElementById('responseDiv');
+    const queryBubble = document.createElement('div');
+    queryBubble.className = 'queryBubble';
+    queryBubble.textContent = `You: ${query}`;
+    responseDiv.appendChild(queryBubble);
+  }
+
 function showResponse(response) {
     const responseDiv = document.getElementById('responseDiv');
-    responseDiv.textContent = response.result  ; // Set the text content to the response string
+    const responseBubble = document.createElement('div');
+    responseBubble.className = 'responseBubble';
+    responseBubble.textContent = `Bot: ${response.result}`;
+    responseDiv.appendChild(responseBubble);
 }
